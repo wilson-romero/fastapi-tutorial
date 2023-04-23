06f19b64ca18b0256130a1a7b540c5f1084756f7
Wilson Romero <me@wilsonromero.com>
2023-04-23 17:20:34 -0500

    10.Declare Request Example Data

--- main.py
+++ main.py
@@ -1,69 +1,68 @@
-from typing import List, Optional, Set
+from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
 class Item(BaseModel):
-    name: str
-    description: Optional[str]
-    price: float
-    tax: Optional[float]
-    tags: Set[str] = set()
-    image: Optional[List[Image]]
+    name: str = Field(example="Foo")
+    description: Optional[str] = Field(
+        default=None, example="A very nice Item")
+    price: float = Field(example=35.4)
+    tax: Optional[float] = Field(default=0, example=3.2)
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Foo",
-                "description": "The pretender",
-                "price": 42.0,
-                "tax": 3.2,
-                "tags": ["rock", "metal", "bar"],
-                "image": [
-                    {
-                        "url": "http://example.com/baz.jpg",
-                        "name": "The Foo live"
-                    },
-                    {
-                        "url": "http://example.com/dave.jpg",
-                        "name": "The Baz"
-                    }
-                ]
-            }
-        }
-
-
-class Offer(BaseModel):
-    name: str
-    description: Optional[str]
-    price: float
-    items: List[Item]
-
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: List[Image]):
-    for image in images:
-        print(image.url)
-        print(image.name)
-    return images
-
-
-@app.post("/offers/")
-async def create_offer(offer: Offer):
-    return offer
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "name": "Foo",
+    #             "description": "A very nice Item",
+    #             "price": 35.4,
+    #             "tax": 3.2,
+    #         }
+    #     }
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(
+    *,
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
     results = {"item_id": item_id, "item": item}
     return results
 
