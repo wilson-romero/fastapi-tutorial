115ac128dcb9b2d877b5b6955b6d0842525e65f3
Wilson Romero <me@wilsonromero.com>
2023-04-23 15:14:14 -0500

    07.Body - Multiple Parameters

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import List, Optional
 
 import uvicorn
-from fastapi import FastAPI, Path, Query
+from fastapi import Body, FastAPI, Path, Query
 from pydantic import BaseModel
 from typing_extensions import Annotated
 
@@ -10,18 +10,40 @@ from typing_extensions import Annotated
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    *,
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1, le=1000)],
-    q: Annotated[Optional[str], Query(alias="item-query")] = None,
-    size: Annotated[float, Query(gt=0, lt=10.5)] = None
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if size:
-        results.update({"size": size})
+class Item(BaseModel):
+    name: str
+    description: Optional[str]
+    price: float
+    tax: Optional[float]
+
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str]
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+#     q: Optional[str] = None,
+#     item: Optional[Item] = None,
+#     user: Optional[User] = None,
+#     importance: Annotated[int, Body(gt=0)] = 5
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     if user:
+#         results.update({"user": user})
+#     if importance:
+#         results.update({"importance": importance})
+#     return results
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=False)]):
+    results = {"item_id": item_id, "item": item}
     return results
 
 if __name__ == "__main__":
