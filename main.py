10c27853acda921437af1b4f6586c1c9d414d447
Wilson Romero <me@wilsonromero.com>
2023-04-23 17:11:12 -0500

    09.Body - Nested Models

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
-from typing import Optional
+from typing import List, Optional, Set
 
 import uvicorn
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
-from typing_extensions import Annotated
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
 class Item(BaseModel):
     name: str
-    description: Optional[str] = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(
-        gt=0, description="The price must be greater than zero")
+    description: Optional[str]
+    price: float
     tax: Optional[float]
+    tags: Set[str] = set()
+    image: Optional[List[Image]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "The pretender",
+                "price": 42.0,
+                "tax": 3.2,
+                "tags": ["rock", "metal", "bar"],
+                "image": [
+                    {
+                        "url": "http://example.com/baz.jpg",
+                        "name": "The Foo live"
+                    },
+                    {
+                        "url": "http://example.com/dave.jpg",
+                        "name": "The Baz"
+                    }
+                ]
+            }
+        }
+
+
+class Offer(BaseModel):
+    name: str
+    description: Optional[str]
+    price: float
+    items: List[Item]
+
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: List[Image]):
+    for image in images:
+        print(image.url)
+        print(image.name)
+    return images
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
 
