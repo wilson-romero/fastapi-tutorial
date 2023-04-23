c8e25f0d0f66fdedd02b4f79bf09936c27361d6e
Wilson Romero <me@wilsonromero.com>
2023-04-23 17:28:15 -0500

    11.Extra Data Types

--- main.py
+++ main.py
@@ -1,70 +1,33 @@
+from datetime import datetime, time, timedelta
 from typing import Optional
+from uuid import UUID
 
 import uvicorn
 from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
 from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str = Field(example="Foo")
-    description: Optional[str] = Field(
-        default=None, example="A very nice Item")
-    price: float = Field(example=35.4)
-    tax: Optional[float] = Field(default=0, example=3.2)
-
-    # class Config:
-    #     schema_extra = {
-    #         "example": {
-    #             "name": "Foo",
-    #             "description": "A very nice Item",
-    #             "price": 35.4,
-    #             "tax": 3.2,
-    #         }
-    #     }
-
-
 @app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int,
-    item: Annotated[
-        Item,
-        Body(
-            examples={
-                "normal": {
-                    "summary": "A normal example",
-                    "description": "A **normal** item works correctly.",
-                    "value": {
-                        "name": "Foo",
-                        "description": "A very nice Item",
-                        "price": 35.4,
-                        "tax": 3.2,
-                    },
-                },
-                "converted": {
-                    "summary": "An example with converted data",
-                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
-                    "value": {
-                        "name": "Bar",
-                        "price": "35.4",
-                    },
-                },
-                "invalid": {
-                    "summary": "Invalid data is rejected with an error",
-                    "value": {
-                        "name": "Baz",
-                        "price": "thirty five point four",
-                    },
-                },
-            },
-        ),
-    ],
+async def read_items(
+    item_id: UUID,
+    start_datetime: Annotated[Optional[datetime], Body()] = None,
+    end_datetime: Annotated[Optional[datetime], Body()] = None,
+    repeat_at: Annotated[Optional[time], Body()] = None,
+    process_after: Annotated[Optional[timedelta], Body()] = None,
 ):
-    results = {"item_id": item_id, "item": item}
-    return results
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
