fecb54243f302ea8382b7e45fea5fdff7cb55273
Wilson Romero <me@wilsonromero.com>
2023-04-24 17:52:24 -0500

    24.04 Dependencies in path operation decorators

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 
-from typing import Optional
-
 import uvicorn
-from fastapi import Cookie, Depends, FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException
 from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-def query_extractor(q: Optional[str] = None):
-    return q
+async def verify_token(x_token: Annotated[str, Header()]):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-def query_or_cookie_extractor(
-    q: Annotated[str, Depends(query_extractor)],
-    last_query: Annotated[Optional[str], Cookie()] = None,
-):
-    if not q:
-        return last_query
-    return q
+async def verify_key(x_key: Annotated[str, Header()]):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
 
 
-@app.get("/items/")
-async def read_query(
-    query_or_default: Annotated[str, Depends(query_or_cookie_extractor)]
-):
-    return {"q_or_cookie": query_or_default}
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
