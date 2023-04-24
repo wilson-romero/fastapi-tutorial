1c2b33e4fc5763c6501a476df8aef91b34f74e85
Wilson Romero <me@wilsonromero.com>
2023-04-23 22:46:46 -0500

    18.Request Files

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 
+from typing import List, Optional
+
 import uvicorn
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-@app.post("/login/")
-async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"username": username}
+@app.post("/file/")
+async def create_file(file: Annotated[Optional[bytes], File(description="A file read as bytes")] = None):
+    if not file:
+        return {"message": "No file sent"}
+    else:
+        return {"file_size": len(file)}
 
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: Annotated[Optional[UploadFile], File(description="A file read as UploadFile")] = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        return {"filename": file.filename}
+
+
+@app.post("/files/")
+async def create_files(files: Annotated[List[bytes], File(description="Multiple files as bytes")]):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: Annotated[
+    List[UploadFile], File(description="Multiple files as UploadFile")
+]):
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+        <body>
+        <form action="/files/" enctype="multipart/form-data" method="post">
+            <input name="files" type="file" multiple>
+            <input type="submit">
+        </form>
+        <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+        <input name="files" type="file" multiple>
+        <input type="submit">
+        </form>
+        </body>
+    """
+    return HTMLResponse(content=content)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
