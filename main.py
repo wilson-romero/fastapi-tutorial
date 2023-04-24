690b29f95bed6f7fd15b6016c7364660f30f70ef
Wilson Romero <me@wilsonromero.com>
2023-04-24 14:22:29 -0500

    22.JSON Compatible Encoder

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
 
+from datetime import datetime
 from typing import Optional, Set
 
 import uvicorn
-from fastapi import FastAPI, status
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 app = FastAPI()
 
+fake_db = {}
+
 
 class Item(BaseModel):
-    name: str
+    title: str
+    timestamp: datetime
     description: Optional[str]
-    price: float
-    tax: Optional[float]
-    tags: Set[str] = set()
 
 
-# @app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED, tags=["items"], summary="Create an item",
-#           description="Create an item with all the information, name, description, price, tax and a set of unique tags")
-@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED, tags=["items"], summary="Create an item",
-          response_description="The created item")
-async def create_item(item: Item):
-    """
-    Create an item with all the information:
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
+    return fake_db[id]
 
-    - **name**: each item must have a name
-    - **description**: a long description
-    - **price**: required
-    - **tax**: if the item doesn't have tax, you can omit this
-    - **tags**: a set of unique tag strings for this item
-    """
-    return item
-
-
-@app.get("/items/", tags=["items"])
-async def read_items():
-    return [{"name": "Foo", "price": 42}]
-
-
-@app.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "johndoe"}]
-
-@app.get("/elements/", tags=["items"], deprecated=True)
-async def read_elements():
-    return [{"item_id": "Foo"}]
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
