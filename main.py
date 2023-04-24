373802fb5fda969d8f3c73fc374649803fff83c2
Wilson Romero <me@wilsonromero.com>
2023-04-24 14:35:58 -0500

    23.Body - Updates

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
-from datetime import datetime
-from typing import Optional, Set
+from typing import List, Optional
 
 import uvicorn
 from fastapi import FastAPI
@@ -9,21 +8,42 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-fake_db = {}
-
 
 class Item(BaseModel):
-    title: str
-    timestamp: datetime
+    name: Optional[str]
     description: Optional[str]
+    price: Optional[float]
+    tax: Optional[float] = 10.5
+    tags: List[str] = []
 
 
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
-    return fake_db[id]
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
 
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
+
+
+@app.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.dict(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
