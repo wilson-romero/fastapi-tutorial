81b59742f9cc7038cba69a92204a7cd50570eadc
Wilson Romero <me@wilsonromero.com>
2023-04-24 17:13:26 -0500

    24.01 Dependencies - First Steps

--- main.py
+++ main.py
@@ -1,49 +1,30 @@
 
-from typing import List, Optional
+from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI
+from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: Optional[str]
-    description: Optional[str]
-    price: Optional[float]
-    tax: Optional[float] = 10.5
-    tags: List[str] = []
+async def common_parameters(
+    q: Optional[str] = None, skip: int = 0, limit: int = 100
+):
+    return {"q": q, "skip": skip, "limit": limit}
+
+CommonsDep = Annotated[dict, Depends(common_parameters)]
+
+@app.get("/items/")
+async def read_items(commons: CommonsDep):
+# async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
 
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
-
-
-@app.get("/items/{item_id}", response_model=Item)
-async def read_item(item_id: str):
-    return items[item_id]
-
-
-@app.put("/items/{item_id}", response_model=Item)
-async def update_item(item_id: str, item: Item):
-    update_item_encoded = jsonable_encoder(item)
-    items[item_id] = update_item_encoded
-    return update_item_encoded
-
-
-@app.patch("/items/{item_id}", response_model=Item)
-async def update_item(item_id: str, item: Item):
-    stored_item_data = items[item_id]
-    stored_item_model = Item(**stored_item_data)
-    update_data = item.dict(exclude_unset=True)
-    updated_item = stored_item_model.copy(update=update_data)
-    items[item_id] = jsonable_encoder(updated_item)
-    return updated_item
+@app.get("/users/")
+async def read_users(commons: CommonsDep):
+# async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
