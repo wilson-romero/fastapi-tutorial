74cf3417a7ccd319fb4fc2cda77676fab09c96a0
Wilson Romero <me@wilsonromero.com>
2023-04-23 22:06:12 -0500

    14.Response Model - Return Type

--- main.py
+++ main.py
@@ -1,20 +1,102 @@
-from typing import List, Optional
+from typing import Any, List, Optional
 
 import uvicorn
-from fastapi import FastAPI, Header
-from typing_extensions import Annotated
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse, RedirectResponse
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(user_agent: Annotated[Optional[str], Header()] = None,
-                     strange_header: Annotated[Optional[str], Header(
-                         convert_underscores=False)] = None,
-                     x_token: Annotated[Optional[List[str]], Header()] = None):
-    return {"User-Agent": user_agent,
-            "strange_header": strange_header,
-            "X-Token values": x_token}
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = 10.5
+    tags: List[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+# class UserIn(BaseModel):
+#     username: str
+#     password: str
+#     email: EmailStr
+#     full_name: Optional[str]
+
+# class UserOut(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: Optional[str]
+
+
+# # Don't do this in production!
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user: UserIn) -> Any:
+#     return user
+
+class BaseUser(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str]
+
+
+class UserIn(BaseUser):
+    password: str
+
+
+@app.post("/user/")
+async def create_user(user: UserIn) -> BaseUser:
+    return user
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
+
+
+@app.get("/items/", response_model=List[Item])
+async def read_items() -> Any:
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
+
+
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+    response_model_include={"name", "description"},
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+async def read_item_public_data(item_id: str):
+    return items[item_id]
+
+
+@app.get("/portal")
+async def get_portal(teleport: bool = False) -> Response:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return JSONResponse(content={"message": "Here's your interdimensional portal."})
+
+
+@app.get("/teleport")
+async def get_teleport() -> RedirectResponse:
+    return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
