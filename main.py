b7b9ff4170081b38959997de89050640ef2c15cb
Wilson Romero <me@wilsonromero.com>
2023-04-23 22:49:40 -0500

    19.Request Forms and Files

--- main.py
+++ main.py
@@ -1,57 +1,22 @@
 
-from typing import List, Optional
-
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, File, Form, UploadFile
 from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-@app.post("/file/")
-async def create_file(file: Annotated[Optional[bytes], File(description="A file read as bytes")] = None):
-    if not file:
-        return {"message": "No file sent"}
-    else:
-        return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: Annotated[Optional[UploadFile], File(description="A file read as UploadFile")] = None):
-    if not file:
-        return {"message": "No upload file sent"}
-    else:
-        return {"filename": file.filename}
-
-
 @app.post("/files/")
-async def create_files(files: Annotated[List[bytes], File(description="Multiple files as bytes")]):
-    return {"file_sizes": [len(file) for file in files]}
-
-
-@app.post("/uploadfiles/")
-async def create_upload_files(files: Annotated[
-    List[UploadFile], File(description="Multiple files as UploadFile")
-]):
-    return {"filenames": [file.filename for file in files]}
-
-
-@app.get("/")
-async def main():
-    content = """
-        <body>
-        <form action="/files/" enctype="multipart/form-data" method="post">
-            <input name="files" type="file" multiple>
-            <input type="submit">
-        </form>
-        <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-        <input name="files" type="file" multiple>
-        <input type="submit">
-        </form>
-        </body>
-    """
-    return HTMLResponse(content=content)
+async def create_file(
+    file: Annotated[bytes, File()],
+    fileb: Annotated[UploadFile, File()],
+    token: Annotated[str, Form()],
+):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
