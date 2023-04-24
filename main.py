e812566d594e7b59b2992c7f16e7425e474a4701
Wilson Romero <me@wilsonromero.com>
2023-04-23 22:16:33 -0500

    15.Extra Models

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-from typing import Any, List, Optional
+from typing import Dict, List, Optional
 
 import uvicorn
-from fastapi import FastAPI, Response
-from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = 10.5
-    tags: List[str] = []
-
-
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
 
 
 # class UserIn(BaseModel):
@@ -29,74 +13,75 @@ items = {
 #     email: EmailStr
 #     full_name: Optional[str]
 
+
 # class UserOut(BaseModel):
 #     username: str
 #     email: EmailStr
 #     full_name: Optional[str]
 
 
-# # Don't do this in production!
-# @app.post("/user/", response_model=UserOut)
-# async def create_user(user: UserIn) -> Any:
-#     return user
+# class UserInDB(BaseModel):
+#     username: str
+#     hashed_password: str
+#     email: EmailStr
+#     full_name: Optional[str]
 
-class BaseUser(BaseModel):
+
+# Reduce duplication
+class UserBase(BaseModel):
     username: str
     email: EmailStr
     full_name: Optional[str]
 
 
-class UserIn(BaseUser):
+class UserIn(UserBase):
     password: str
 
 
-@app.post("/user/")
-async def create_user(user: UserIn) -> BaseUser:
-    return user
+class UserOut(UserBase):
+    pass
 
 
-@app.post("/items/")
-async def create_item(item: Item) -> Item:
-    return item
+class UserInDB(UserBase):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+
+
+items = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
 
 
 @app.get("/items/", response_model=List[Item])
-async def read_items() -> Any:
-    return [
-        Item(name="Portal Gun", price=42.0),
-        Item(name="Plumbus", price=32.0),
-    ]
+async def read_items():
+    return items
 
 
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
-async def read_item(item_id: str):
-    return items[item_id]
-
-
-@app.get(
-    "/items/{item_id}/name",
-    response_model=Item,
-    response_model_include={"name", "description"},
-)
-async def read_item_name(item_id: str):
-    return items[item_id]
-
-
-@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
-async def read_item_public_data(item_id: str):
-    return items[item_id]
-
-
-@app.get("/portal")
-async def get_portal(teleport: bool = False) -> Response:
-    if teleport:
-        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    return JSONResponse(content={"message": "Here's your interdimensional portal."})
-
-
-@app.get("/teleport")
-async def get_teleport() -> RedirectResponse:
-    return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+@app.get("/keyword-weights/", response_model=Dict[str, float])
+async def read_keyword_weights():
+    return {"foo": 2.3, "bar": 3.4}
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
