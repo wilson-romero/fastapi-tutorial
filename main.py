107f2fd8b0db0ea1017e59e46573c3663073bb1c
Wilson Romero <me@wilsonromero.com>
2023-04-24 17:24:55 -0500

    24.02 Classes as Dependencies

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ from typing_extensions import Annotated
 app = FastAPI()
 
 
-async def common_parameters(
-    q: Optional[str] = None, skip: int = 0, limit: int = 100
-):
-    return {"q": q, "skip": skip, "limit": limit}
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
 
-CommonsDep = Annotated[dict, Depends(common_parameters)]
+
+class CommonQueryParams:
+    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
 
 @app.get("/items/")
-async def read_items(commons: CommonsDep):
-# async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: CommonsDep):
-# async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
+async def read_items(commons: Annotated[CommonQueryParams, Depends()]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
