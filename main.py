79e9ea7d8aebc1b0d036eb151d85c1f77cc86ca1
Wilson Romero <me@wilsonromero.com>
2023-04-24 21:59:44 -0500

    26.Middleware

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
+import time
 from datetime import datetime, timedelta
 from typing import Optional
 
 import uvicorn
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -53,6 +54,15 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 def verify_password(plain_password, hashed_password):
