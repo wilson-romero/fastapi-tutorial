47a036da7dea86040e43dc17be6a49312455b844
Wilson Romero <me@wilsonromero.com>
2023-04-25 07:32:01 -0500

    28.5 SQLModel Filter Data - WHERE

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, col, create_engine, select
 
 
 class Hero(SQLModel, table=True):
@@ -24,21 +24,30 @@ def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America",
+                  secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero)
+        statement = select(Hero).where(col(Hero.age) >= 35)
         results = session.exec(statement)
-        heroes = results.all()
-        print(heroes)
+        for hero in results:
+            print(hero)
 
 
 def main():
